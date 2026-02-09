75290f6fb7875b638d02a45fe913e9d1409e517d
DaCommander31 <millerjr808@gmail.com>
2026-02-09 20:26:34 +1000

    Port to 1.20.2

--- common/src/main/java/dev/dacommander31/smart_pick/mixin/MinecraftMixin.java
+++ common/src/main/java/dev/dacommander31/smart_pick/mixin/MinecraftMixin.java
@@ -37,9 +37,7 @@ public class MinecraftMixin {
         assert player != null;
         Inventory inventory = player.getInventory();
         SmartPickPlatform platform = Platform.get();
-        platform.log("test");
         if (stack.isEmpty() || inventory.contains(stack) || player.getAbilities().instabuild || !platform.isEnabled()) return stack;
-        platform.log("test2");
 
         Map<Item, List<Item>> map = PickBlockCache.getCache();
 
